575026043a737a95d088938f61d7226c1b264f40
umtdrk <96914329+umtdrk@users.noreply.github.com>
2022-11-03 17:48:07 +0300

    Add files via upload

--- src/test/java/HepsiburadaUyeGirisi.java
+++ src/test/java/HepsiburadaUyeGirisi.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class HepsiburadaUyeGirisi {
@@ -21,6 +21,7 @@ public class HepsiburadaUyeGirisi {
     JavascriptExecutor js;
     WebDriverWait wait;
     @Before
+    //Driver kurulumu ve gerekli ayarlamalar yapılır.
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         //avoid-bot-detection
@@ -38,50 +39,53 @@ public class HepsiburadaUyeGirisi {
     }
 
     @Test
-    public void hepsiburadayegirii() throws InterruptedException {
-//        Actions action = new Actions(driver);
-//        action.moveToElement(driver.findElement(By.id("myAccount")));
+    public void sepeteUrunekle() throws InterruptedException {
+        //Üye girişi yapılır.
         driver.findElement(By.id("myAccount")).click();
         driver.findElement(By.id("login")).click();
         driver.findElement(By.id("txtUserName")).sendKeys("umitdrk33@gmail.com");
         driver.findElement(By.id("btnLogin")).click();
         driver.findElement(By.id("txtPassword")).sendKeys("Test123456");
         driver.findElement(By.id("btnEmailSelect")).click();
+        //istenilen ürün aranır.
         driver.findElement(By.className("desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b")).click();
         driver.findElement(By.className("desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b")).sendKeys("sleepy bebek bezi 5 numara");
         driver.findElement(By.className("SearchBoxOld-cHxjyU99nxdIaAbGyX7F")).click();
-
         js.executeScript("window.scrollTo(0,100)");
         Thread.sleep(500);
 
+        //ilk ürün secilir.
         driver.findElement(By.xpath("//*[@id='i0']/div/a/div[1]/div[1]/div[2]")).click();
+        //yeni sekmeye gecilir.
         for (String handle : driver.getWindowHandles())
         { driver.switchTo().window(handle); }
         js.executeScript("window.scrollTo(0,300)");
         Thread.sleep(500);
-
+        //ürün sepete eklenir.
         driver.findElement(By.id("addToCart")).click();
         Thread.sleep(1000);
 
-        driver.findElement(By.className("checkoutui-Modal-2iZXl")).click();
+        driver.findElement(By.className("checkoutui-Modal-iHhyy79iR28NvF33vKJb")).click();
         js.executeScript("window.scrollTo(0,500)");
-
+        //2. urun sepete eklenir ve sepete gidilir.
         driver.findElement(By.cssSelector("tr:nth-child(1) .add-to-basket")).click();
-
         WebElement sepeteGitButton = driver.findElement(By.xpath("//button[@type='button'] [contains(text(),'Sepete git')]"));
         wait.until(ExpectedConditions.elementToBeClickable(sepeteGitButton)).click();
-
-        assertThat(driver.findElement(By.linkText("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet")).getText(), is("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet"));
+        // sepetteki ürünlerin dogru oldugu dogrulanır.
+        assertThat(driver.findElement(By.xpath("(//div[@class='product_name_3Lh3t'])[1]")).getText(), containsString("Bebek Bezi 5 Numara"));
         js.executeScript("window.scrollTo(0,250)");
-        assertThat(driver.findElement(By.linkText("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet")).getText(), is("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet"));
-        //sepeti boşalt
-//        driver.findElement(By.xpath("(//a[@class='product_delete_1zR-0'])[1]")).click();
-//        driver.findElement(By.className("iDSyON ")).click();
-//        Thread.sleep(500);
-//        driver.findElement(By.xpath("(//a[@class='product_delete_1zR-0'])")).click();
+        assertThat(driver.findElement(By.xpath("(//div[@class='product_name_3Lh3t'])[2]")).getText(), containsString("Bebek Bezi 5 Numara"));
+        //sepeti boşaltılır.
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//a[@aria-label='Ürünü Kaldır'])[1]")).click();
+        try {driver.findElement(By.cssSelector("button[kind='secondary']")).click();
+        }catch (Exception e){}
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//a[@aria-label='Ürünü Kaldır']")).click();
     }
     @After
+    //driver sonlandırılır.
     public void tearDown() {
-        //driver.quit();
+        driver.quit();
     }
 }
--- src/test/java/HepsiburadaUyeOlmadan.java
+++ src/test/java/HepsiburadaUyeOlmadan.java
@@ -2,8 +2,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.is;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,6 +20,7 @@ public class HepsiburadaUyeOlmadan {
   JavascriptExecutor js;
   WebDriverWait wait;
   @Before
+  //driver kurulumu ve ayarlamaları yapılır.
   public void setUp() {
     WebDriverManager.chromedriver().setup();
     driver = new ChromeDriver();
@@ -28,39 +30,42 @@ public class HepsiburadaUyeOlmadan {
     wait = new WebDriverWait(driver, 10);
     js = (JavascriptExecutor) driver;
   }
-
   @Test
-  public void hepsiburadayegirii() throws InterruptedException {
+  public void sepeteUrunEkle() throws InterruptedException {
+    //istenilen ürün araması yapılır.
     driver.findElement(By.className("desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b")).click();
     driver.findElement(By.className("desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b")).sendKeys("sleepy bebek bezi 5 numara");
     driver.findElement(By.className("SearchBoxOld-cHxjyU99nxdIaAbGyX7F")).click();
-
+    Thread.sleep(500);
     js.executeScript("window.scrollTo(0,100)");
     Thread.sleep(500);
-
+    //ilk ürün seçilir.
     driver.findElement(By.xpath("//*[@id='i0']/div/a/div[1]/div[1]/div[2]")).click();
+    //yeni acilan sekmeye geçilir.
     for (String handle : driver.getWindowHandles())
     { driver.switchTo().window(handle); }
     js.executeScript("window.scrollTo(0,300)");
     Thread.sleep(500);
-
+    //ürün sepete eklenir.
     driver.findElement(By.id("addToCart")).click();
     Thread.sleep(1000);
 
-    driver.findElement(By.className("checkoutui-Modal-2iZXl")).click();
+    driver.findElement(By.className("checkoutui-Modal-iHhyy79iR28NvF33vKJb")).click();
     js.executeScript("window.scrollTo(0,500)");
-
+    //2. ürün sepete eklenir ve sepete gidilir.
     driver.findElement(By.cssSelector("tr:nth-child(1) .add-to-basket")).click();
 
     WebElement sepeteGitButton = driver.findElement(By.xpath("//button[@type='button'] [contains(text(),'Sepete git')]"));
     wait.until(ExpectedConditions.elementToBeClickable(sepeteGitButton)).click();
-
-    assertThat(driver.findElement(By.linkText("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet")).getText(), is("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet"));
+    //sepetteki ürünlerin doğru olduğu doğrulanır.
+    assertThat(driver.findElement(By.xpath("(//div[@class='product_name_3Lh3t'])[1]")).getText(), containsString("Bebek Bezi 5 Numara"));
     js.executeScript("window.scrollTo(0,250)");
-    assertThat(driver.findElement(By.linkText("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet")).getText(), is("Sleepy Extra Günlük Aktivite Ekonomik Paket Bebek Bezi 5 Numara Junior 40 Adet"));
+    assertThat(driver.findElement(By.xpath("(//div[@class='product_name_3Lh3t'])[2]")).getText(), containsString("Bebek Bezi 5 Numara"));
+    Thread.sleep(1000);
   }
   @After
+  //driver sonlandırılır.
   public void tearDown() {
-    //driver.quit();
+    driver.quit();
   }
 }
